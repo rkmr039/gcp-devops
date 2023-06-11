5c46c99a0e8e2b23aac9ec375179c0d9ee7e3f54
Rishab Kumar <rkmr039@gmail.com>
2023-06-11 16:10:29 +0530

    app change

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world()
+def hello_world():
     return 'Hello'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
